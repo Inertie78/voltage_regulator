623d0e5096a8db55460a18bd8ba3c6c9e43e765a
Goumf <g.leibzig@bluewin.ch>
2025-03-21 21:24:31 +0100

    update i2c

--- script/main.py
+++ script/main.py
@@ -1,53 +1,93 @@
-import board 
-from ina219 import ADCResolution, BusVoltageRange, INA219
+import gpiod
+from gpiod.line import Direction, Value
+from prometheus_client import start_http_server
+import logging
 import time
+import os
+
+from sensor import Sensor
+from infoPc import InfoPc
+from multimetre import Multimetre
 
 
-i2c_bus = board.I2c()
+RELPIN = 23
 
-ina1 = INA219(i2c_bus, addr=0x40)
-ina2 = INA219(i2c_bus, addr=0x41)
-ina3 = INA219(i2c_bus, addr=0x42)
-ina4 = INA219(i2c_bus, addr=0x43)
+rel_line = gpiod.request_lines(
+    "/dev/gpiochip0",
+    consumer="blink-example",
+    config={
+        RELPIN: gpiod.LineSettings(
+            direction=Direction.OUTPUT, output_value=Value.ACTIVE
+        )
+    }
+)
 
-ina1.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
-ina1.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
-ina1.bus_voltage_range = BusVoltageRange.RANGE_16V
-
-ina2.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
-ina2.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
-ina2.bus_voltage_range = BusVoltageRange.RANGE_16V
-
-ina3.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
-ina3.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
-ina3.bus_voltage_range = BusVoltageRange.RANGE_16V
-
-ina4.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
-ina4.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
-ina4.bus_voltage_range = BusVoltageRange.RANGE_16V
+def blinkLed():
+    rel_line.set_value(RELPIN, Value.ACTIVE)
+    time.sleep(10)
+    rel_line.set_value(RELPIN, Value.INACTIVE)
+    time.sleep(10)
 
 
-while True:
-    bus_voltage1 = ina1.bus_voltage        
-    shunt_voltage1 = ina1.shunt_voltage    
-    power1 = ina1.power
-    current1 = ina1.current                
+def main():
+    info_pc = InfoPc()
+    list_sensor = info_pc.get_list_sensor()
 
-    bus_voltage2 = ina2.bus_voltage        
-    shunt_voltage2 = ina2.shunt_voltage    
-    power2 = ina2.power
-    current2 = ina2.current                
-    
-    bus_voltage3 = ina3.bus_voltage        
-    shunt_voltage3 = ina3.shunt_voltage    
-    power3 = ina3.power
-    current3 = ina3.current                
+    multimetre = Multimetre()
 
+    sensors = []
+    for name in list_sensor:
+        try:
+            value_test = float(list_sensor[name])
+            sensor = Sensor(name)
+            sensors.append(sensor)
+        except:
+            pass
 
+    while True:
+        info_pc.infoPc()
+        
+        for sensor in sensors:
+            try:
+                name = sensor.get_name()
+                value = list_sensor[name]
 
-    print("PSU Voltage:{:6.3f}V    Shunt Voltage:{:9.6f}V    Load Voltage:{:6.3f}V    Power:{:9.6f}W    Current:{:9.6f}A".format((bus_voltage1 + shunt_voltage1),(shunt_voltage1),(bus_voltage1),(power1),(current1/1000)))
-    print("PSU Voltage:{:6.3f}V    Shunt Voltage:{:9.6f}V    Load Voltage:{:6.3f}V    Power:{:9.6f}W    Current:{:9.6f}A".format((bus_voltage2 + shunt_voltage2),(shunt_voltage2),(bus_voltage2),(power2),(current2/1000)))
-    print("PSU Voltage:{:6.3f}V    Shunt Voltage:{:9.6f}V    Load Voltage:{:6.3f}V    Power:{:9.6f}W    Current:{:9.6f}A".format((bus_voltage3 + shunt_voltage3),(shunt_voltage3),(bus_voltage3),(power3),(current3/1000)))
-    print("")
-    print("")
-    time.sleep(1)
+                logging.info(f"{name}: {value}")
+                
+                sensor.set_gauge(value)
+            
+            except Exception as e:
+                logging.error(f"An error occurred when assigning values to the gauges: {e}")
+
+        
+        bus_voltage1, bus_voltage2, bus_voltage3, bus_voltage4 = multimetre.get_bus_voltage()
+        shunt_voltage1, shunt_voltage2, shunt_voltage3, shunt_voltage4 = multimetre.get_shunt_voltage()
+        power1, power2, power3, power4 = multimetre.get_power()
+        current1, current2, current3, current4 = multimetre.get_power()
+
+        print("")
+        print("")
+
+        print("PSU Voltage:{:6.3f} [V]    Shunt Voltage:{:9.6f} [V]    Load Voltage:{:6.3f} [V]   Power:{:9.6f}W    Current:{:9.6f} [A]".format((bus_voltage1 + shunt_voltage1),(shunt_voltage1),(bus_voltage1),(power1),(current1/1000)))
+        print("PSU Voltage:{:6.3f} [V]    Shunt Voltage:{:9.6f} [V]    Load Voltage:{:6.3f} [V]   Power:{:9.6f}W    Current:{:9.6f} [A]".format((bus_voltage2 + shunt_voltage2),(shunt_voltage2),(bus_voltage2),(power2),(current2/1000)))
+        print("PSU Voltage:{:6.3f} [V]    Shunt Voltage:{:9.6f} [V]    Load Voltage:{:6.3f} [V]   Power:{:9.6f}W    Current:{:9.6f} [A]".format((bus_voltage3 + shunt_voltage3),(shunt_voltage3),(bus_voltage3),(power3),(current3/1000)))
+        print("PSU Voltage:{:6.3f} [V]    Shunt Voltage:{:9.6f} [V]    Load Voltage:{:6.3f} [V]   Power:{:9.6f}W    Current:{:9.6f} [A]".format((bus_voltage4 + shunt_voltage4),(shunt_voltage4),(bus_voltage4),(power4),(current4/1000)))
+        print("")
+        print("")
+
+        time.sleep(60)
+        #blinkLed()
+    else:
+        led_line.release()
+
+if __name__ == "__main__":
+    format = "%(asctime)s %(levelname)s: %(message)s"
+    level = os.getenv("LOG_LEVEL", "INFO")
+    logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+    port = int(os.getenv("EXPORTER_PORT", 8000))
+    logging.info(f"Starting web server at port {port}")
+    start_http_server(port)
+
+    main()
---
+++ script/multimetre.py
@@ -0,0 +1,43 @@
+import board 
+import busio
+from adafruit_ina219 import ADCResolution, BusVoltageRange, INA219
+
+class Multimetre() :
+    '''Classe qui va permettre d'interragir avec le multimètre'''
+    def __init__(self, line=1) :
+        
+        i2c_bus = busio.I2C(board.SCL, board.SDA)
+
+        self.ina1 = INA219(i2c_bus, addr=0x40)
+        self.ina2 = INA219(i2c_bus, addr=0x41)
+        self.ina3 = INA219(i2c_bus, addr=0x42)
+        self.ina4 = INA219(i2c_bus, addr=0x43)
+
+        self.ina1.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+        self.ina1.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+        self.ina1.bus_voltage_range = BusVoltageRange.RANGE_16V 
+
+        self.ina2.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+        self.vina2.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+        self.ina2.bus_voltage_range = BusVoltageRange.RANGE_16V
+
+        self.ina3.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+        self.ina3.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+        self.ina3.bus_voltage_range = BusVoltageRange.RANGE_16V
+
+        self.ina4.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+        self.ina4.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_32S
+        self.ina4.bus_voltage_range = BusVoltageRange.RANGE_16V
+
+                
+    def get_bus_voltage(self):
+        return (self.ina1.bus_voltage , self.ina2.bus_voltage , self.ina3.bus_voltage , self.ina4.bus_voltage)
+    
+    def get_shunt_voltage(self):
+        return (self.ina1.shunt_voltage , self.ina2.shunt_voltage , self.ina3.shunt_voltage , self.ina4.shunt_voltage)
+    
+    def get_power(self):
+        return (self.ina1.power , self.ina2.power , self.ina3.power , self.ina4.power)
+    
+    def get_current(self):
+        return (self.ina1.current , self.ina2.current , self.ina3.current , self.ina4.current)
