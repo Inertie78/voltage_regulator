6fd5fbce731660b0747b18686acd756757bee808
Inertie78 <debonschristophe@gmail.com>
2025-03-27 17:47:30 +0100

    update exemple/managingTime.py

--- exemple/managingTime.py
+++ exemple/managingTime.py
@@ -10,12 +10,12 @@ while True:
     current_time = time.time()
 
 
-    # Envoie le dictionaire au container flask. Une seule fois 5 secondes apés le démarrage. 
+   
     if (current_time - last_time_01 > TIME_VALUE_01):
         print(f'Sa fait {current_time - last_time_01} second (selon la varibale TIME_VALUE_01 = {TIME_VALUE_01}) en date du {time.ctime()}')
         last_time_01 = current_time
 
-    # Envoie le dictionaire au container flask. Une seule fois 5 secondes apés le démarrage. 
+
     if (current_time - last_time_02 > TIME_VALUE_02):
         print(f'Sa fait {current_time - last_time_02} second (selon la varibale TIME_VALUE_01 = {TIME_VALUE_02}) en date du {time.ctime()}')
         last_time_02 = current_time
