288682c076c323ad7c05d4a225ac461af129fdfc
Inertie78 <debonschristophe@gmail.com>
2025-04-09 19:45:58 +0200

    mise à jour page flask multimetre et relay

--- flask/static/script_multimeter.js
+++ flask/static/script_multimeter.js
@@ -1,33 +1,94 @@
+var myObj = null
 document.addEventListener("DOMContentLoaded", () => {
-  socket.send("up_bat");
-  socket.on('message', function(msg) {
-    if (msg != null) {
-      if(msg.includes("bat_")){
-        myObj = JSON.parse(msg);
-        $("#psu_voltage_01").text(Number(myObj.bat_psu_voltage_01).toFixed(3) + " V");
-        $("#bus_voltage_01").text(Number(myObj.bat_bus_voltage_01).toFixed(3) + " V");
-        $("#shunt_voltage_01").text(Number(myObj.bat_shunt_voltage_01).toFixed(3) + " V");
-        $("#current_01").text(Number(myObj.bat_current_01).toFixed(3) + " A");
-        $("#power_01").text(Number(myObj.bat_power_01).toFixed(3) + " W");
-        
-        $("#psu_voltage_02").text(Number(myObj.bat_psu_voltage_02).toFixed(3) + " V");
-        $("#bus_voltage_02").text(Number(myObj.bat_bus_voltage_02).toFixed(3) + " V");
-        $("#shunt_voltage_02").text(Number(myObj.bat_shunt_voltage_02).toFixed(3) + " V");
-        $("#current_02").text(Number(myObj.bat_current_02).toFixed(3) + " A");
-        $("#power_02").text(Number(myObj.bat_power_02).toFixed(3) + " W");
 
-        $("#psu_voltage_03").text(Number(myObj.bat_psu_voltage_03).toFixed(3) + " V");
-        $("#bus_voltage_03").text(Number(myObj.bat_bus_voltage_03).toFixed(3) + " V");
-        $("#shunt_voltage_03").text(Number(myObj.bat_shunt_voltage_03).toFixed(3) + " V");
-        $("#current_03").text(Number(myObj.bat_current_03).toFixed(3) + " A");
-        $("#power_03").text(Number(myObj.bat_power_03).toFixed(3) + " W");
-        
-        $("#psu_voltage_04").text(Number(myObj.bat_psu_voltage_04).toFixed(3) + " V");
-        $("#bus_voltage_04").text(Number(myObj.bat_bus_voltage_04).toFixed(3) + " V");
-        $("#shunt_voltage_04").text(Number(myObj.bat_shunt_voltage_04).toFixed(3) + " V");
-        $("#current_04").text(Number(myObj.bat_current_04).toFixed(3) + " A");
-        $("#power_04").text(Number(myObj.bat_power_04).toFixed(3) + " W");
-      }
-    }
-  });
+  google.charts.load('current', { 'packages': ['gauge'] });
+  google.charts.setOnLoadCallback(drawChart);
+
 });
+
+function drawChart() {
+
+  var chartTension_01 = new google.visualization.Gauge(document.getElementById("chart_Tension_01"));
+  var chartTension_02 = new google.visualization.Gauge(document.getElementById("chart_Tension_02"));
+  var chartTension_03 = new google.visualization.Gauge(document.getElementById("chart_Tension_03"));
+  var chartTension_04 = new google.visualization.Gauge(document.getElementById("chart_Tension_04"));
+
+  var dataTension = google.visualization.arrayToDataTable([
+    ['Label', 'Value'],
+    ['Tens', 11]
+  ]);
+FULL_CHARGE_TENSION = 12.8
+CYCLE_CHARGE_TENSION = 12.4
+MIN_CHARGE_TENSION = 11.9
+  var optionsTension = {
+    width: 150, height: 150,
+    min: 11, max: 14,
+    redFrom: 11.5, redTo: 11.9,
+    yellowFrom: 11.9, yellowTo: 12.4,
+    greenFrom: 12.4, greenTo: 12.8,
+    majorTicks: ['11', '12', '13', '14'],
+    minorTicks: 10
+  };
+
+  var formatTension = new google.visualization.NumberFormat({
+    suffix: ' V',
+    fractionDigits: 1
+  });
+
+  
+  formatTension.format(dataTension, 1);
+  chartTension_01.draw(dataTension, optionsTension);
+  chartTension_02.draw(dataTension, optionsTension);
+  chartTension_03.draw(dataTension, optionsTension);
+  chartTension_04.draw(dataTension, optionsTension);
+
+  setInterval(function () {
+    socket.send("up_bat");
+    socket.on('message', function(msg) {
+      if (msg != null) {
+        if(msg.includes("bat_")){
+          formatTension.format(dataTension, 1);
+          myObj = JSON.parse(msg);
+          var voltage = Number(myObj.bat_shunt_voltage_01).toFixed(3)
+          $("#shunt_voltage_01").text( voltage + " V");
+          dataTension.setValue(0, 1, voltage);
+          chartTension_01.draw(dataTension, optionsTension);
+          $("#current_01").text( Number(myObj.bat_current_01).toFixed(3) + " A");
+          $("#power_01").text( Number(myObj.bat_power_01).toFixed(3) + " W");
+          
+          voltage = Number(myObj.bat_shunt_voltage_02).toFixed(3)
+          $("#shunt_voltage_02").text( voltage + " V");
+          dataTension.setValue(0, 1, voltage);
+          chartTension_02.draw(dataTension, optionsTension);
+          $("#current_02").text( Number(myObj.bat_current_02).toFixed(3) + " A");
+          $("#power_02").text( Number(myObj.bat_power_02).toFixed(3) + " W");
+
+          voltage = Number(myObj.bat_shunt_voltage_03).toFixed(3)
+          $("#shunt_voltage_03").text( voltage + " V");
+          dataTension.setValue(0, 1, voltage);
+          chartTension_03.draw(dataTension, optionsTension);
+          $("#current_03").text( Number(myObj.bat_current_03).toFixed(3) + " A");
+          $("#power_03").text( Number(myObj.bat_power_03).toFixed(3) + " W");
+          
+          voltage = Number(myObj.bat_shunt_voltage_04).toFixed(3)
+          $("#shunt_voltage_04").text( voltage + " V");
+          dataTension.setValue(0, 1, voltage);
+          chartTension_04.draw(dataTension, optionsTension);
+          $("#current_04").text( Number(myObj.bat_current_04).toFixed(3) + " A");
+          $("#power_04").text( Number(myObj.bat_power_04).toFixed(3) + " W");
+        }
+      }
+    });
+
+      
+      
+
+      
+      
+      
+
+      
+
+
+  }, (1000));
+}
--- flask/static/script_relay.js
+++ flask/static/script_relay.js
@@ -1,116 +1,122 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-  socket.send("up_relay");
-  socket.on('message', function(msg) {
-    if (msg != null) {
-      if(msg.includes("rs_0")){
-        myObj = JSON.parse(msg);
-        if (Object.keys(myObj).length > 1){
-          const etat_switch = document.querySelectorAll('input');
-          const etat_text = document.querySelectorAll('h3');
-          for (let i = 0; i < etat_switch.length; i++) {
-            etat_switch[i].checked = myObj[etat_switch[i].name]
-            textEtatChexbox(etat_switch[i].name, etat_switch[i].checked);
-          }
-          
-        }
-      }
-    }
-  });
+  
 
   // Select all input elements on the page
   const inputs = document.querySelectorAll('input');
-
-  // Function to handle input changes
-  function handleInputChange(event) {
-    const name = event.target.name;
-    const checked = event.target.checked;
-    let etat_switch = {};
-
-    if(name.includes("au_")){
-      if(name === 'au_ob'){
-        $("#au_pr").prop('checked', false);
-        $("#au_co").prop('checked', false);
-        $("#au_ma").prop('checked', false);
-        etat_switch[name] = checked;
-        Object.assign(etat_switch, { "au_pr": false });
-        Object.assign(etat_switch, { "au_co": false });
-        Object.assign(etat_switch, { "au_ma": false });
-        Object.assign(etat_switch, { "au_ma": false });
-      }else if(name === 'au_pr'){
-        $("#au_co").prop('checked', false);
-        $("#au_ob").prop('checked', false);
-        $("#au_ma").prop('checked', false);
-        etat_switch[name] = checked;
-        Object.assign(etat_switch, { "au_co": false });
-        Object.assign(etat_switch, { "au_ob": false });
-        Object.assign(etat_switch, { "au_ma": false });
-      }else if(name === 'au_co'){
-        $("#au_pr").prop('checked', false);
-        $("#au_ob").prop('checked', false);
-        $("#au_ma").prop('checked', false);
-        etat_switch[name] = checked;
-        Object.assign(etat_switch, { "au_pr": false });
-        Object.assign(etat_switch, { "au_ob": false });
-        Object.assign(etat_switch, { "au_ma": false });
-      }else if(name === 'au_ma'){
-        $("#au_pr").prop('checked', false);
-        $("#au_ob").prop('checked', false);
-        $("#au_co").prop('checked', false);
-        etat_switch[name] = checked;
-        Object.assign(etat_switch, { "au_pr": false });
-        Object.assign(etat_switch, { "au_ob": false });
-        Object.assign(etat_switch, { "au_co": false });
-
-        $("#rs_01").prop('checked', false);
-        $("#rs_02").prop('checked', false);
-        $("#rs_03").prop('checked', false);
-        $("#rs_04").prop('checked', false);
-
-        Object.assign(etat_switch, { "rs_01": false });
-        Object.assign(etat_switch, { "rs_02": false });
-        Object.assign(etat_switch, { "rs_03": false });
-        Object.assign(etat_switch, { "rs_04": false });
-      }
-      
-    }else{
-      etat_switch[name] = checked;
-    }
-
-    textEtatChexbox(name, checked);
-
-    const jsonString = JSON.stringify(etat_switch);
-    socket.send(jsonString);
-  }
-
-  
 
   // Add event listener to each input element
   inputs.forEach(input => {
       input.addEventListener('input', handleInputChange);
   });
 
-  function textEtatChexbox(name, checked) {
-    const etat_text = document.querySelectorAll('h3');
-    for (let i = 0; i < etat_text.length; i++) {
-      let mainString = etat_text[i].id
-      if (mainString.includes(name)) {
-        if(checked === true){
-          etat_text[i].textContent = 'Etat close'
-        }else{
-          etat_text[i].textContent = 'Etat open'
-        }
-      }  
-    }
-
-  }
-
-  function activateCheckbox(clickedCheckbox) {
-    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-    checkboxes.forEach(checkbox => {
-        checkbox.checked = false;
-    });
-    clickedCheckbox.checked = true;
-  }
+  updatePage();
   
 });
+
+// Function to handle input changes
+function handleInputChange(event) {
+  const name = event.target.name;
+  const checked = event.target.checked;
+  let etat_switch = {};
+
+  if(name.includes("au_")){
+    if(name === 'au_ob'){
+      $("#au_pr").prop('checked', false);
+      $("#au_co").prop('checked', false);
+      $("#au_ma").prop('checked', false);
+      etat_switch[name] = checked;
+      Object.assign(etat_switch, { name: checked });
+      Object.assign(etat_switch, { "au_pr": false });
+      Object.assign(etat_switch, { "au_co": false });
+      Object.assign(etat_switch, { "au_ma": false });
+    }else if(name === 'au_pr'){
+      $("#au_co").prop('checked', false);
+      $("#au_ob").prop('checked', false);
+      $("#au_ma").prop('checked', false);
+      etat_switch[name] = checked;
+      Object.assign(etat_switch, { "au_co": false });
+      Object.assign(etat_switch, { "au_ob": false });
+      Object.assign(etat_switch, { "au_ma": false });
+    }else if(name === 'au_co'){
+      $("#au_pr").prop('checked', false);
+      $("#au_ob").prop('checked', false);
+      $("#au_ma").prop('checked', false);
+      etat_switch[name] = checked;
+      Object.assign(etat_switch, { "au_pr": false });
+      Object.assign(etat_switch, { "au_ob": false });
+      Object.assign(etat_switch, { "au_ma": false });
+    }else if(name === 'au_ma'){
+      $("#au_pr").prop('checked', false);
+      $("#au_ob").prop('checked', false);
+      $("#au_co").prop('checked', false);
+      etat_switch[name] = checked;
+      Object.assign(etat_switch, { "au_pr": false });
+      Object.assign(etat_switch, { "au_ob": false });
+      Object.assign(etat_switch, { "au_co": false });
+
+      $("#rs_01").prop('checked', false);
+      $("#rs_02").prop('checked', false);
+      $("#rs_03").prop('checked', false);
+      $("#rs_04").prop('checked', false);
+
+      Object.assign(etat_switch, { "rs_01": false });
+      Object.assign(etat_switch, { "rs_02": false });
+      Object.assign(etat_switch, { "rs_03": false });
+      Object.assign(etat_switch, { "rs_04": false });
+    }
+    
+    if($("#au_co").prop("checked") === false && $("#au_pr").prop("checked") === false && $("#au_ma").prop("checked") === false && $("#au_ob").prop("checked") === false){
+      $("#au_ob").prop('checked', true);
+      Object.assign(etat_switch, { "au_ob": true });
+      Object.assign(etat_switch, { "au_pr": false });
+      Object.assign(etat_switch, { "au_co": false });
+      Object.assign(etat_switch, { "au_ma": false });
+    }
+    
+  }else{
+    etat_switch[name] = checked;
+  }
+
+  textEtatChexbox(name, checked);
+
+  const jsonString = JSON.stringify(etat_switch);
+  socket.send(jsonString);
+}
+
+function textEtatChexbox(name, checked) {
+  const etat_text = document.querySelectorAll('h3');
+  for (let i = 0; i < etat_text.length; i++) {
+    let mainString = etat_text[i].id
+    if (mainString.includes(name)) {
+      if(checked === true){
+        etat_text[i].textContent = 'Etat close'
+      }else{
+        etat_text[i].textContent = 'Etat open'
+      }
+    }  
+  }
+
+}
+
+function updatePage() {
+  setInterval(function () {
+    socket.send("up_relay");
+    socket.on('message', function(msg) {
+      if (msg != null) {
+        if(msg.includes("rs_0")){
+          myObj = JSON.parse(msg);
+          if (Object.keys(myObj).length > 1){
+            const etat_switch = document.querySelectorAll('input');
+            const etat_text = document.querySelectorAll('h3');
+            for (let i = 0; i < etat_switch.length; i++) {
+              etat_switch[i].checked = myObj[etat_switch[i].name]
+              textEtatChexbox(etat_switch[i].name, etat_switch[i].checked);
+            }
+            
+          }
+        }
+      }
+    });
+  }, (1000));
+}
 
