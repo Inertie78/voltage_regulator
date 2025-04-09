85dcc19874e4034264cf62ac7434d644fa278682
Inertie78 <debonschristophe@gmail.com>
2025-04-09 17:06:59 +0200

    Update script_multimeter.js

--- flask/static/script_multimeter.js
+++ flask/static/script_multimeter.js
@@ -4,29 +4,29 @@ document.addEventListener("DOMContentLoaded", () => {
     if (msg != null) {
       if(msg.includes("bat_")){
         myObj = JSON.parse(msg);
-        $("#psu_voltage_01").text(myObj.bat_psu_voltage_01 + " V");
-        $("#bus_voltage_01").text(myObj.bat_bus_voltage_01 + " V");
-        $("#shunt_voltage_01").text(myObj.bat_shunt_voltage_01 + " V");
-        $("#current_01").text(myObj.bat_current_01 + " A");
-        $("#power_01").text(myObj.bat_power_01 + " W");
+        $("#psu_voltage_01").text(Number(myObj.bat_psu_voltage_01).toFixed(3) + " V");
+        $("#bus_voltage_01").text(Number(myObj.bat_bus_voltage_01).toFixed(3) + " V");
+        $("#shunt_voltage_01").text(Number(myObj.bat_shunt_voltage_01).toFixed(3) + " V");
+        $("#current_01").text(Number(myObj.bat_current_01).toFixed(3) + " A");
+        $("#power_01").text(Number(myObj.bat_power_01).toFixed(3) + " W");
         
-        $("#psu_voltage_02").text(myObj.bat_psu_voltage_02 + " V");
-        $("#bus_voltage_02").text(myObj.bat_bus_voltage_02 + " V");
-        $("#shunt_voltage_02").text(myObj.bat_shunt_voltage_02 + " V");
-        $("#current_02").text(myObj.bat_current_02 + " A");
-        $("#power_02").text(myObj.bat_power_02 + " W");
+        $("#psu_voltage_02").text(Number(myObj.bat_psu_voltage_02).toFixed(3) + " V");
+        $("#bus_voltage_02").text(Number(myObj.bat_bus_voltage_02).toFixed(3) + " V");
+        $("#shunt_voltage_02").text(Number(myObj.bat_shunt_voltage_02).toFixed(3) + " V");
+        $("#current_02").text(Number(myObj.bat_current_02).toFixed(3) + " A");
+        $("#power_02").text(Number(myObj.bat_power_02).toFixed(3) + " W");
 
-        $("#psu_voltage_03").text(myObj.bat_psu_voltage_03 + " V");
-        $("#bus_voltage_03").text(myObj.bat_bus_voltage_03 + " V");
-        $("#shunt_voltage_03").text(myObj.bat_shunt_voltage_03 + " V");
-        $("#current_03").text(myObj.bat_current_03 + " A");
-        $("#power_03").text(myObj.bat_power_03 + " W");
+        $("#psu_voltage_03").text(Number(myObj.bat_psu_voltage_03).toFixed(3) + " V");
+        $("#bus_voltage_03").text(Number(myObj.bat_bus_voltage_03).toFixed(3) + " V");
+        $("#shunt_voltage_03").text(Number(myObj.bat_shunt_voltage_03).toFixed(3) + " V");
+        $("#current_03").text(Number(myObj.bat_current_03).toFixed(3) + " A");
+        $("#power_03").text(Number(myObj.bat_power_03).toFixed(3) + " W");
         
-        $("#psu_voltage_04").text(myObj.bat_psu_voltage_04 + " V");
-        $("#bus_voltage_04").text(myObj.bat_bus_voltage_04 + " V");
-        $("#shunt_voltage_04").text(myObj.bat_shunt_voltage_04 + " V");
-        $("#current_04").text(myObj.bat_current_04 + " A");
-        $("#power_04").text(myObj.bat_power_04 + " W");
+        $("#psu_voltage_04").text(Number(myObj.bat_psu_voltage_04).toFixed(3) + " V");
+        $("#bus_voltage_04").text(Number(myObj.bat_bus_voltage_04).toFixed(3) + " V");
+        $("#shunt_voltage_04").text(Number(myObj.bat_shunt_voltage_04).toFixed(3) + " V");
+        $("#current_04").text(Number(myObj.bat_current_04).toFixed(3) + " A");
+        $("#power_04").text(Number(myObj.bat_power_04).toFixed(3) + " W");
       }
     }
   });
